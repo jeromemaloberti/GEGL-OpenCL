21d655213644eb46e09ecb979cd8b23bc89c1290
Jerome Maloberti <jmaloberti@gmail.com>
2016-02-10 00:08:59 +0000

    Add invert-gamma in opencl.

--- operations/common/invert-gamma.c
+++ operations/common/invert-gamma.c
@@ -62,6 +62,53 @@ process (GeglOperation       *op,
   return TRUE;
 }
 
+#include "opencl/invert-gamma.cl.h"
+
+static GeglClRunData *cl_data = NULL;
+
+static gboolean
+cl_process (GeglOperation       *op,
+            cl_mem               in_tex,
+            cl_mem               out_tex,
+            const size_t         samples,
+            const GeglRectangle *roi,
+            gint                 level)
+{
+	if(!cl_data)
+		{
+      const char *kernel_name[] = {"gegl_invert_gamma", NULL};
+      cl_data = gegl_cl_compile_and_build(invert_gamma_cl_source, kernel_name);
+
+		}
+	
+   if(!cl_data)
+      return TRUE;
+   else
+   {
+      cl_int cl_err = 0;
+      const size_t global_ws[2] = {samples, 1};
+
+      cl_err = gegl_cl_set_kernel_args(cl_data->kernel[0],
+                                      sizeof(cl_mem), (void *)&in_tex,
+                                      sizeof(cl_mem), (void *)&out_tex,
+                                      NULL);
+
+      cl_err = gegl_clEnqueueNDRangeKernel(gegl_cl_get_command_queue(),
+                                           cl_data->kernel[0], 2, NULL,
+                                           global_ws, NULL, 0, NULL, NULL);
+      CL_CHECK;
+
+      cl_err = gegl_clFinish(gegl_cl_get_command_queue());
+      CL_CHECK;
+
+      return FALSE;
+
+error:
+      return TRUE;
+   }
+}
+
+
 static void
 gegl_op_class_init (GeglOpClass *klass)
 {
@@ -74,6 +121,9 @@ gegl_op_class_init (GeglOpClass *klass)
   operation_class->prepare     = prepare;
   point_filter_class->process  = process;
 
+	point_filter_class->cl_process = cl_process;
+	operation_class->opencl_support = TRUE;
+
   gegl_operation_class_set_keys (operation_class,
     "name"       , "gegl:invert-gamma",
     "title",      _("Invert in Perceptual space"),
@@ -81,6 +131,7 @@ gegl_op_class_init (GeglOpClass *klass)
     "description",
        _("Inverts the components (except alpha), the result is the "
          "corresponding \"negative\" image."),
+		"cl-source", invert_gamma_cl_source,
     NULL);
 }
 
